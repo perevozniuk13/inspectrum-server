db9f060065169547e5942cc743123839f34aea67
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-15 15:04:15 +0100

    add adding and editing palettes endpoints

--- controllers/palette-controller.js
+++ controllers/palette-controller.js
@@ -9,6 +9,38 @@ const index = async (_req, res) => {
   }
 };
 
+const addPalette = async (req, res) => {
+  if (
+    (!req.body.colour1, !req.body.colour2, !req.body.colour3, !req.body.colour4)
+  ) {
+    return res.status(400).send("All colours must be provided!");
+  }
+
+  try {
+    const update = await knex("palettes").insert(req.body);
+    const updatedPalettesData = await knex("palettes");
+    res.status(200).json(updatedPalettesData);
+  } catch (error) {
+    res.status(400).send("Error adding palette!");
+  }
+};
+
+const editPalette = async (req, res) => {
+  const { paletteId } = req.params;
+
+  try {
+    const update = await knex("palettes")
+      .where({ id: paletteId })
+      .update(req.body);
+    const updatedPaletteData = await knex("palettes").where({ id: paletteId });
+    res.status(200).json(updatedPaletteData);
+  } catch (error) {
+    res.status(400).send("Error updating palette!");
+  }
+};
+
 module.exports = {
   index,
+  addPalette,
+  editPalette,
 };
--- routes/palettes.js
+++ routes/palettes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const paletteController = require("../controllers/palette-controller");
 
-
-
 router.route("/").get(paletteController.index);
+
+router.route("/").post(paletteController.addPalette);
+
+router.route("/:paletteId").put(paletteController.editPalette);
 
 module.exports = router;

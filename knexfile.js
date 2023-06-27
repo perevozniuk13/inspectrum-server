139ae9497ab049fd4df580872e6a9b0a7730d2bf
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-27 20:59:05 +0100

    fix env variables

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
-  client: 'mysql',
+  client: "mysql",
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'rootroot',
-    database: 'inspectrum_db',
-    charset: 'utf8',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    charset: "utf8",
   },
 };
